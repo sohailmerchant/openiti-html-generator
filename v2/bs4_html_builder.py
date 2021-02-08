691f03b30dac5f4625f0b1554792b5021b7e3fbf
pverkind <peter.verkinderen@gmail.com>
2021-02-08 13:22:59 +0100

    Clean up and document

--- v2/bs4_html_builder.py
+++ v2/bs4_html_builder.py
@@ -4,165 +4,173 @@ import os
 from bs4 import BeautifulSoup
 # load repl list and replace functions from config.py: 
 from config import *
-from utility import *
+from utility import get_book_metadata, get_book_files
 
-base_path = 'template/'
+from openiti.helper.funcs import get_all_text_files_in_folder
+
 
 #path = r"/mnt/c/Development Work/0400AH/data/"
 path = r"/mnt/c/Development Work/0400AH/data/test/"
+path = "test"
 book_id = ''
 
-def select_files(path):
+def convert_text(s):
+    """Convert an OpenITI text to html \
+    using the `repl` list of patterns and replacements in the config.py file
 
-    for root, dirs, files in os.walk(path):
-        print(root)
-     
-        for name in files:
-            
-            if re.search('^\d{4}\w+\.\w+\.\w+-[a-z]{3}\d{1}(\.(mARkdown|inProgress|completed))?$',name):
-            
-                book_name = name
-                print(book_name)
-                book_id = name.split(".")[2].split("-")[0]
-                print(book_id)
-                with open(root+'/'+name,encoding='utf-8') as f:
-                    data = f.read()
+    Args:
+        s (str): text in OpenITI mARkdown format
 
-                def convert_text(s):
-                        """Convert an OpenITI text to html \
-                        using the `repl` list of patterns and replacements in the config.py file
+    Returns:
+        s (str): text with html markup
+    """
+    for d in repl: # loaded from config.py
+        if "flags" in d:
+            s = re.sub(d["ptrn"], d["repl"], s, flags=d["flags"])
+        else:
+            s = re.sub(d["ptrn"], d["repl"], s)
+    return s
 
-                        Args:
-                            s (str): text in OpenITI mARkdown format
+def toc_to_ul(toc):
+    """Convert the table of contents into a collapsible list
 
-                        Returns:
-                            s (str): text with html markup
-                        """
-                        for d in repl: # loaded from config.py
-                            if "flags" in d:
-                                s = re.sub(d["ptrn"], d["repl"], s, flags=d["flags"])
-                            else:
-                                s = re.sub(d["ptrn"], d["repl"], s)
-                        return s
+    See http://code.iamkate.com/javascript/collapsible-lists/
+    """
+    ul = "<div class='card-header title-heading'>عنوان الفصل</div><ul class='collapsibleList'>\n "
+    level = 1
+    for a in toc:
+        lev = int(re.findall("l(\d+)", a)[0])
+        if lev > level:
+            while lev > level:
+                ul += "{}<ul class='collapsibleList'>\n".format(" "*level*4)
+                level += 1
+        elif lev < level:
+            while lev < level:
+                ul += "\n{}</li>".format(" "*level*4)
+                level -= 1
+                ul += "\n{}</ul>\n".format(" "*level*4)
 
-                def toc_to_ul(toc):
-                    """Convert the table of contents into a collapsible list
+        ul += "{0}<li class='collapsibleListClosed'>{1}\n".format(" "*level*4, a)
+    while level > 0:
+        ul += "\n{}</li>".format(" "*level*4)
+        level -= 1
+        ul += "\n{}</ul>\n".format(" "*level*4)
+    
+    return ul
 
-                    See http://code.iamkate.com/javascript/collapsible-lists/
-                    """
-                    ul = "<div class='card-header title-heading'>عنوان الفصل</div><ul class='collapsibleList'>\n "
-                    level = 1
-                    for a in toc:
-                        lev = int(re.findall("l(\d+)", a)[0])
-                        if lev > level:
-                            while lev > level:
-                                ul += "{}<ul class='collapsibleList'>\n".format(" "*level*4)
-                                level += 1
-                        elif lev < level:
-                            while lev < level:
-                                ul += "\n{}</li>".format(" "*level*4)
-                                level -= 1
-                                ul += "\n{}</ul>\n".format(" "*level*4)
+def toc_panel(html):
+    """Create the table of contents panel
 
-                        ul += "{0}<li class='collapsibleListClosed'>{1}\n".format(" "*level*4, a)
-                    while level > 0:
-                        ul += "\n{}</li>".format(" "*level*4)
-                        level -= 1
-                        ul += "\n{}</ul>\n".format(" "*level*4)
-                    
-                    return ul
+    Args:
+        html (str): OpenITI text converted to html
+    """
+    toc = []
+    for level, title in re.findall("<h\d+ .+?l(\d+).+?id='([^']+)", html):
+        tag = """
+                    <a class='toc-item l{0}' href='#{1}'>{1}</a>"""
+        toc.append(tag.format(level, title))
+    toc = toc_to_ul(toc)
+    
+    return toc
+    
+def html_builder(s, meta, template_path):
+    """Build the body of the html file
 
-                def toc_panel(html):
-                    """Create the table of contents panel
+    Args:
+        s (str): OpenITI text as string
+        meta (dict): dictionary containing the metadata of the book
+    """
+    s = convert_text(s)
+    
+    # create right navigation panel: 
+    right = toc_panel(s)
+ 
+    with open(template_path, 'r') as html:
+        contents = html.read()
 
-                    Args:
-                        html (str): OpenITI text converted to html
-                    """
-                    toc = []
-                    for level, title in re.findall("<h\d+ .+?l(\d+).+?id='([^']+)", html):
-                        tag = """
-                                    <a class='toc-item l{0}' href='#{1}'>{1}</a>"""
-                        toc.append(tag.format(level, title))
-                    toc = toc_to_ul(toc)
-                    
-                    return toc
-                    
-                def html_builder(s, meta):
-                    
-                    
-                    """Build the body of the html file
+        soup = BeautifulSoup(contents, 'lxml')
 
-                    Args:
-                        s (str): OpenITI text as string
-                        uri (str): OpenITI version URI of the text
-                    """
-                    s = convert_text(s)
-                    
-                    # create right navigation panel: 
-                    right = toc_panel(s)
-                 
-                    with open(base_path + 'main.html', 'r') as html:
+        
+        right_div = soup.find(id='sidebar-wrapper')
+        book_main = soup.find(id='content')
+        metadata = soup.find(id='metadata-content')
 
-                        contents = html.read()
+        for key, value in meta.items():
 
-                        soup = BeautifulSoup(contents, 'lxml')
+            new_p = soup.new_tag("label")
+            value = key + ": " + value
+            new_p.append((value))                           
+            metadata.insert(0, new_p)
+        
+        soup.new_tag("div", right_div.append(BeautifulSoup(right, 'lxml')))
+        soup.new_tag("div", book_main.insert(1, BeautifulSoup(s, 'html.parser')))
 
-                        
-                        right_div = soup.find(id='sidebar-wrapper')
-                        book_main = soup.find(id='content')
-                        metadata = soup.find(id='metadata-content')
+        
+       
+    # format main text as html:
+    
+    full_html = soup
+    return str(full_html)
 
-                        for key, value in meta.items():
+def create_html_file(html_str, outfp):
+    print("SAVING AS", outfp)
 
-                            new_p = soup.new_tag("label")
-                            value = key + ": " + value
-                            new_p.append((value))                           
-                            metadata.insert(0, new_p)
-                        
-                        soup.new_tag("div", right_div.append(BeautifulSoup(right, 'lxml')))
-                        soup.new_tag("div", book_main.insert(1, BeautifulSoup(s, 'html.parser')))
+    ## fix the path issue
+    with open(outfp, "w", encoding="utf-8") as e:
+        e.write(html_str)
 
-                        
-                       
-                    # format main text as html:
-                    
-                    full_html = soup
-                    
-                    
-                    
+def get_metadata_by_id(book_id, meta_fp):
+    """Get the metadata for the book by book id
 
-                    return str(full_html)
+    Args:
+        book_id (str): Book Id  as string
+    """
+    book_details = get_book_metadata(book_id, meta_fp)
+    print(book_details)
+    title = book_details['title_lat']
+    title_ar = book_details['title_ar']
+    author_date = book_details['date']
+    author_ar = book_details['author_ar']
+    author_eng = book_details['author_lat']
+    #print(title)
 
-                def create_html_file(html_str):
-                    print("SAVING AS", book_name + ".html")
-
-                    ## fix the path issue
-                    with open(book_name + ".html", "w", encoding="utf-8") as e:
-                        e.write(html_str)
-
-                def get_metadata_by_id(book_id):
-                    """Get the metadata for the book by book id
-
-                    Args:
-                        book_id (str): Book Id  as string
-                    """
-                    book_details = get_book_metadata(book_id)
-                    #title = book_details['title_lat']
-                    title_ar = book_details['title_ar']
-                    author_date = book_details['date']
-                    author_ar = book_details['author_ar']
-                    author_eng = book_details['author_lat']
-                    #print(title)
-
-                    return book_details
-
-                s = data
-                
-                # get the metadata of the book by id
-                meta = get_metadata_by_id(book_id)
-
-                html_str = html_builder(s, meta)
-                create_html_file(html_str)
+    return book_details   
 
 
-select_files(path)
+def convert_all_files_in_folder(folder, meta_fp='metadata.csv', template_path="template/main.html"):
+    """Convert all text files in the folder (and its subfolders) into html.
+
+    Args:
+        folder (str): path to folder containing the (subfolders with) text files
+        meta_fp (str): path to the file containing the metadata
+    """
+
+##    for root, dirs, files in os.walk(path):
+##        print(root)
+##     
+##        for name in files:
+##            
+##            if re.search('^\d{4}\w+\.\w+\.\w+-[a-z]{3}\d{1}(\.(mARkdown|inProgress|completed))?$',name):
+##            
+##                version_uri = name
+##                print(version_uri)
+##                book_id = name.split(".")[2].split("-")[0]
+##                print(book_id)
+##                with open(root+'/'+name,encoding='utf-8') as f:
+##                    data = f.read()
+    for fp in get_all_text_files_in_folder(folder):
+        version_uri = re.split(r"[/\\]", fp)[-1]
+        print(version_uri)
+        book_id = version_uri.split(".")[2].split("-")[0]
+        print(book_id)
+        with open(fp, encoding='utf-8') as f:
+            s = f.read()
+        
+        # get the metadata of the book by id
+        meta = get_metadata_by_id(book_id, meta_fp)
+
+        html_str = html_builder(s, meta, template_path=template_path)
+        create_html_file(html_str, version_uri+".html")
+
+
+convert_all_files_in_folder(path)
